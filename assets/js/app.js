f4df25808adffe67da807833301e01205c106045
Dilshodbek <dilshodjongulomov53@gmail.com>
2023-02-18 09:38:09 +0500

    done

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+// navbar
+let menu = document.querySelector("#menu-bar")
+let navItems = document.querySelector(".nav-items")
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times")
+  navItems.classList.toggle("active")
+}
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times")
+  navItems.classList.remove("active")
+}
+
+// slider
+let slides = document.querySelectorAll(".slide-container")
+let index = 0
+
+function next() {
+  slides[index].classList.remove("active")
+  index = (index + 1) % slides.length
+  slides[index].classList.add("active")
+  console.log(1)
+}
+
+function prev() {
+  slides[index].classList.remove("active")
+  index = (index - 1 + slides.length) % slides.length
+  slides[index].classList.add("active")
+}
+
+// boots
+document.querySelectorAll(".featured-image-1").forEach((image_1) => {
+  image_1.addEventListener('click', () => {
+    var src = image_1.getAttribute('src')
+    document.querySelector(".big-image-1").src = src;
+  })
+})
+
+document.querySelectorAll(".featured-image-2").forEach((image_2) => {
+  image_2.addEventListener("click", () => {
+    var src = image_2.getAttribute("src")
+    document.querySelector(".big-image-2").src = src
+  })
+})
+
+document.querySelectorAll(".featured-image-3").forEach((image_3) => {
+  image_3.addEventListener("click", () => {
+    var src = image_3.getAttribute("src")
+    document.querySelector(".big-image-3").src = src
+  })
+})
